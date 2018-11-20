f18de632470b1a1cbfeab59291e90c3991cfb013
ccomai92 <ccomai92@gmail.com>
2018-11-20 00:53:59 -0800

    train and data preprocessing done

---
+++ dataset.py
@@ -0,0 +1,152 @@
+"""
+Class to read the input data.
+reads cvs file from provided training and testing data folders.
+"""
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+
+class DataSet(object):
+    def __init__(self, img_labels, images, classes):
+        self._num_examples = img_labels.shape[0]
+        self._img_labels = img_labels
+        self._images = images
+
+        self._cls = classes
+        self._epochs_done = 0
+        self._index_in_epoch = 0
+
+    def num_examples(self):
+        return self._num_examples
+
+    def img_labels(self):
+        return self._img_labels
+
+    def images(self):
+        return self._images
+
+    def epochs_done(self):
+        return self._epochs_done
+
+    def cls(self):
+        return self._cls
+
+    def next_batch(self, batch_size):
+        """Return the next 'batch_size' examples from this data set"""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+
+        if self._index_in_epoch > self._num_examples:
+            # After each epoch we update this
+            self._epochs_done += 1
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+
+        return self._img_labels[start:end], self._images[start:end], self._cls[start:end]
+
+
+
+
+def load_train(train_path, fonts):
+    ch_pixels = []
+    ch_names = []
+    classes = []
+
+    # print(fonts)
+    for font in fonts[:40]:
+        path = os.path.join(train_path, font)
+        data_frame = pd.read_csv(path)
+        data = data_frame.values    # table
+
+        for d in data:
+            # character in integer
+            if (d[2] >= 65 and d[2] <= 90):
+                classes.append(d[2])
+
+                # pixels in 20 * 20 * 3
+                pixels = d[-400:].reshape(20, 20)
+                result = np.zeros((20, 20, 3), dtype=int)
+                temp = pixels[:, :]
+                # r = (temp >> 5) * 32
+                # g = ((temp & 28) >> 2) * 32
+                # b = (temp & 3) * 64
+
+                r = temp
+                g = temp
+                b = temp
+                result[:, :, 0] = r
+                result[:, :, 1] = g
+                result[:, :, 2] = b
+
+                ch_pixels.append(result)
+
+            # ch_names
+
+    unique_classes = np.unique(classes)
+    for character_int in classes:
+        temp = np.zeros(unique_classes.size)
+        #index = np.where(classes == character_int)
+        #print(character_int)
+        index = character_int - 65
+        temp[index] = 1.0
+        print(temp)
+        ch_names.append(temp)
+
+    ch_names = np.array(ch_names)
+    ch_pixels = np.array(ch_pixels)
+    classes = np.array(classes)
+        #ch_names.append(names)
+        #ch_pixels.append(result)
+        #font_labels.append(font[:-4])
+
+        #ch_names = np.array(ch_names)
+        #ch_pixels = np.array(ch_pixels)
+        #font_labels = np.array(font_labels)
+        #print(ch_pixels)
+
+    #print(ch_names)
+    #print(classes)
+
+
+    #print(type(ch_names))
+    #print(result)
+    return ch_names, ch_pixels, classes
+
+def read_train_sets(train_path, fonts, validation_size):
+    class DataSets(object):
+        pass
+    data_sets = DataSets()
+
+    ch_names, ch_pixels, classes = load_train(train_path, fonts)
+    ch_names, ch_pixels, classes = shuffle(ch_names, ch_pixels, classes)
+    if isinstance(validation_size, float):
+        validation_size = int(validation_size * ch_names.shape[0])
+
+    validation_images = ch_pixels[:validation_size]
+    validation_labels = ch_names[:validation_size]
+    validation_classes = classes[:validation_size]
+
+    train_images = ch_pixels[validation_size:]
+    train_labels = ch_names[validation_size:]
+    train_classes = classes[validation_size:]
+
+    data_sets.train = DataSet(train_labels, train_images, train_classes)
+    data_sets.valid = DataSet(validation_labels, validation_images, validation_classes)
+    return data_sets
+
+
+# Unit Test
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    train_path = 'Dataset'
+    fonts = os.listdir(train_path)
+    #print(fonts)
+    ch_names, ch_pixels, classes = load_train(train_path, fonts)
+    print(ch_names[0])
+    plt.imshow(ch_pixels[0])
+    plt.show()
+
---
+++ train.py
@@ -0,0 +1,195 @@
+import dataset
+import tensorflow as tf
+import time
+from datetime import timedelta
+import math
+import random
+import numpy as np
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Adding seed so that random initializaiton is consistent
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(2)
+
+# Prepare input data
+classes = os.listdir('Dataset')
+
+# CONSTANTS
+# 20% of data wil be automatically be used for validation
+VALIDATION_SIZE = 0.1
+NUM_CHANNELS = 3
+TRAIN_PATH = 'Dataset'
+IMG_SIZE = 20
+
+# Reading input data
+data = dataset.read_train_sets(TRAIN_PATH, classes, VALIDATION_SIZE)
+
+print("Complete Reading input data. ")
+print("Number of images in training-set:\t{}".format(len(data.train.img_labels())))
+print("Number of labels in validation-set:\t{}".format(len(data.valid.img_labels())))
+
+
+session = tf.Session()
+x = tf.placeholder(tf.float32, shape=[None, IMG_SIZE, IMG_SIZE, NUM_CHANNELS], name='x')
+
+## labels
+# unique font type (no duplicates)
+num_classes = data.train.img_labels()[0].size
+
+# storing predictions
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+## Network graph params
+filter_size_conv1 = 3
+num_filters_conv1 = 32
+
+filter_size_conv2 = 3
+num_filters_conv2 = 32
+
+filter_size_conv3 = 3
+num_filters_conv3 = 64
+
+# Fully connected layer
+fc_layer_size = 128
+
+def create_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+def create_biases(size):
+    return tf.Variable(tf.constant(0.05, shape=[size]))
+
+
+def create_convolutional_layer(input, num_input_channels,
+                            conv_filter_size, num_filters):
+    # define the weights that will be trained
+    weights = create_weights(shape=[conv_filter_size, conv_filter_size, num_input_channels, num_filters])
+
+    # create biases
+    biases = create_biases(num_filters)
+
+    # creating convolutional layer
+    layer = tf.nn.conv2d(input=input, filter=weights, strides=[1, 1, 1, 1], padding='SAME')
+    layer += biases
+
+    # max-pooling
+    layer = tf.nn.max_pool(value=layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # Output of pooling is fed to Relu
+    layer = tf.nn.relu(layer)
+
+    return layer
+
+def create_flatten_layer(layer):
+    # shape of the layer [batch_size, img_size, img_size, num_channels]
+    layer_shape = layer.get_shape()
+
+    ## num of features will be img_height * img_width * num_channels
+    num_features = layer_shape[1:4].num_elements()
+
+    ## flatten the layer
+    layer = tf.reshape(layer, [-1, num_features])
+
+    return layer
+
+# fully connected layer
+def create_fc_layer(input, num_inputs, num_outputs, use_relu=True):
+    # define trainable weights and biases
+    weights = create_weights(shape=[num_inputs, num_outputs])
+    biases = create_biases(num_outputs)
+
+    # fc layer takes input x and produce wx + b
+    layer = tf.matmul(input, weights) + biases
+    if use_relu:
+        layer = tf.nn.relu(layer)
+    return layer
+
+layer_conv1 = create_convolutional_layer(input=x,
+                num_input_channels=NUM_CHANNELS,
+                conv_filter_size=filter_size_conv1,
+                num_filters=num_filters_conv1)
+
+layer_conv2 = create_convolutional_layer(input=layer_conv1,
+                num_input_channels=num_filters_conv1,
+                conv_filter_size=filter_size_conv2,
+                num_filters=num_filters_conv2)
+
+layer_conv3 = create_convolutional_layer(input=layer_conv2,
+                num_input_channels=num_filters_conv2,
+                conv_filter_size=filter_size_conv3,
+                num_filters=num_filters_conv3)
+
+layer_flat = create_flatten_layer(layer_conv3)
+
+layer_fc1 = create_fc_layer(input=layer_flat,
+                    num_inputs=layer_flat.get_shape()[1:4].num_elements(),
+                    num_outputs=fc_layer_size,
+                    use_relu=True)
+
+layer_fc2 = create_fc_layer(input=layer_fc1,
+                    num_inputs=fc_layer_size,
+                    num_outputs=num_classes,
+                    use_relu=False)
+
+# contains the predicted probability of each class for
+# each input image
+y_pred = tf.nn.softmax(layer_fc2, name='y_pred')
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+
+# cross_entropy
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
+                                                        labels=y_true)
+# average cross_entropy
+cost = tf.reduce_mean(cross_entropy)
+
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+
+# run optimizer operation inside session.run(), inorder to calculate
+# the whole network will have to be run and we will pass the training image
+# in a feed_dict
+
+BATCH_SIZE = 32
+
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+session.run(tf.global_variables_initializer())
+
+# y_true_batch is BATCH_SIZE of labels
+# x_batch is BATCH_SIZE of images
+def show_progress(epoch, feed_dict_train, feed_dict_validate, val_loss):
+    acc = session.run(accuracy, feed_dict=feed_dict_train)
+    val_acc = session.run(accuracy, feed_dict=feed_dict_validate)
+    msg = "Training Epoch {0} --- Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%},  Validation Loss: {3:.3f}"
+    print(msg.format(epoch + 1, acc, val_acc, val_loss))
+
+total_iterations = 0
+
+saver=tf.train.Saver()
+def train(num_iterations):
+    global total_iterations
+
+    for i in range(total_iterations, total_iterations + num_iterations):
+
+        y_true_batch, x_batch, cls_batch = data.train.next_batch(BATCH_SIZE)
+        y_valid_batch, x_valid_batch, valid_cls_batch = data.valid.next_batch(BATCH_SIZE)
+
+        feed_dict_tr = {x: x_batch, y_true: y_true_batch}
+        feed_dict_val = {x: x_valid_batch, y_true: y_valid_batch}
+
+        session.run(optimizer, feed_dict=feed_dict_tr)
+
+        if i % int(data.train.num_examples() / BATCH_SIZE) == 0:
+            val_loss = session.run(cost, feed_dict=feed_dict_val)
+            epoch = int(i / int(data.train.num_examples() / BATCH_SIZE))
+
+            show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
+            saver.save(session, './characters_model')
+
+    total_iterations += num_iterations
+
+train(num_iterations=3000)
